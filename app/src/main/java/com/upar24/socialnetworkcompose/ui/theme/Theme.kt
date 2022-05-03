6dc90ca29023f227b8ee69facdc0b4ce22ea901a
Upar24 <alfionita.fina24@gmail.com>
2022-05-03 10:11:50 +0700

    Initial commit

---
+++ app/src/main/java/com/upar24/socialnetworkcompose/ui/theme/Theme.kt
@@ -0,0 +1,28 @@
+package com.upar24.socialnetworkcompose.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = GreenAccent,
+    background = DarkGray,
+    onBackground = TextWhite,
+    onPrimary= DarkGray,
+    surface = MediumGray,
+    onSurface = LightGray
+)
+
+
+@Composable
+fun SocialNetworkComposeTheme(content: @Composable() () -> Unit) {
+    MaterialTheme(
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
